a20771a0fde4a126b9aad117941aae8a5ef8bc2b
Fabi <fabicaneyu@gmail.com>
2020-03-31 16:38:50 -0300

    Aula 2

---
+++ Aula3_01-04-2020/script.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    
+
+
+    
+    document.getElementById("demo").innerHTML = "";
+
+}
+
+function medFunction(){
+
+
+
+
+    document.getElementById("med").innerHTML = " ";  
+}
+
+function idaFunction(){
+
+
+
+    document.getElementById("de").innerHTML = " ";  
+}
+
+
+
+
