8961ae811d7f879a524e6e52f5bd6300e7799636
Fabi <fabicaneyu@gmail.com>
2020-03-31 21:59:07 -0300

    Resolução Aula 2

--- Aula2_31-03-2020/script.js
+++ Aula2_31-03-2020/script.js
@@ -1,22 +1,25 @@
+
 function myFunction() {
-    document.getElementById("peso");
-    document.getElementById("altura");
-    document.getElementById("demo").innerHTML = "meu IMC é "+(peso/(altura*altura));
+    var formulario = document.getElementById("formulario");
+
+    var kilos = +formulario.kilos.value;
+    var altura = +formulario.altura.value;
+    var imc = ((kilos / (altura * altura)));
+
+    document.getElementById("demo").innerHTML = "Seu IMC é "+imc.toFixed(2);  
 }
 
 function medFunction(){
-
-
-
-
-    document.getElementById("med").innerHTML = " ";  
+    document.getElementById("med").innerHTML = " A média de idade da família do Guilherme é "+ ((30+20+15+10)/4);  
 }
 
 function idaFunction(){
+    var form = document.getElementById("form");
+    var ano = +form.ano.value;
+    var atual = +form.atual.value;
+    var edad = atual - ano; 
 
-
-
-    document.getElementById("de").innerHTML = " ";  
+    document.getElementById("de").innerHTML = "A sua idade é "+ edad;  
 }
 
 
